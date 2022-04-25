236ff9ea474c89b20d5cd4fcbf05470a8725bcf9
meechew <carl.f.bunt@gmail.com>
2022-04-25 12:10:34 -0500

    got work to do but making head way.

--- Capstone/src/server/APIcall.js
+++ Capstone/src/server/APIcall.js
@@ -2,47 +2,65 @@ const fetch = require( "node-fetch");
 const FormData = require('form-data');
 const dotenv = require('dotenv');
 dotenv.config();
-const apikey = process.env.API_KEY;
+const geoUser = process.env.GEO_USER;
+const wthrkey = process.env.WTHR_KEY;
+const pixakey = process.env.PIXA_KEY;
 
 
 class APIcall {
 
     constructor(input, date) {
-        this.pixaForm = new FormData();
-        this.pixaForm.append('key', apikey);
-        this.pixaForm.append('lang', 'en');
-        this.pixaForm.append('url', input);
-
         this.geoForm = new FormData();
-        this.geoForm.append('key', apikey);
+        this.geoForm.append('key', geoUser);
         this.geoForm.append('lang', 'en');
-        this.geoForm.append('url', input);
+        this.geoForm.append('input', input);
 
         this.weatherForm = new FormData();
-        this.weatherForm.append('key', apikey);
+        this.weatherForm.append('key', wthrkey);
         this.weatherForm.append('lang', 'en');
         this.weatherForm.append('url', input);
 
+        this.pixaForm = new FormData();
+        this.pixaForm.append('key', pixakey);
+        this.pixaForm.append('lang', 'en');
+        this.pixaForm.append('url', input);
+
         this.date = date;
-    }
 
-    async call() {
+        async function geoCall() {
+            let url = 'http://api.geonames.org/postalCodeSearch?placename=' +
+                '${input}&username=${geoUser}';
+            const response = await fetch(url, {
+                method: 'POST',
+            })
 
-        const response = await fetch("https://api.meaningcloud.com/sentiment-2.1", {
-            method: 'POST',
-            body: this.form,
-            redirect: 'follow'
-        })
-        try {
-            const data = await response.json();
-            console.log(data);
-            return data;
         }
-        catch(error) {
-            console.log('error', error);
+
+        async function wthrCall() {
+
         }
+        async function pixaCall() {
+
+        }
+
+        this.call = async function() {
+
+            const response = await fetch("https://api.meaningcloud.com/sentiment-2.1", {
+                method: 'POST',
+                body: this.form,
+                redirect: 'follow'
+            })
+            try {
+                const data = await response.json();
+                console.log(data);
+                return data;
+            }
+            catch(error) {
+                console.log('error', error);
+            }
 
             return response;
+        }
     }
 
 }
